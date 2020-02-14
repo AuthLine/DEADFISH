6ffa2046d4c2bec58a65dec24114ac52c11ea657
AuthLine <107609648+AuthLine@users.noreply.github.com>
2020-02-14 21:16:01 +0000

    Release v1.0.1

--- core/mod.permissions.js
+++ core/mod.permissions.js
@@ -167,4 +167,322 @@ const default_perm = {
       'remote'
     ]
   },
-  'output:status': 
+  'output:status': {
+    'standard': [
+      'local',
+      'remote'
+    ],
+    'provider': [
+      'local',
+      'remote'
+    ]
+  },
+  'permissions:add': {
+    'standard': [
+      'local'
+    ],
+    'provider': [
+      'local'
+    ]
+  },
+  'permissions:delete': {
+    'standard': [
+      'local'
+    ],
+    'provider': [
+      'local'
+    ]
+  },
+  'permissions:get': {
+    'standard': [
+      'local'
+    ],
+    'provider': [
+      'local'
+    ]
+  },
+  'permissions:set_type': {
+    'standard': [
+      'local'
+    ],
+    'provider': [
+      'local'
+    ]
+  },
+  'permissions:reset': {
+    'standard': [
+      'local'
+    ],
+    'provider': [
+      'local'
+    ]
+  },
+  'permissions:set': {
+    'standard': [
+      'local'
+    ],
+    'provider': [
+      'local'
+    ]
+  },
+  'settings:set': {
+    'standard': [
+      'local'
+    ],
+    'provider': [
+      'local'
+    ]
+  },
+  'signals:add_admin': {
+    'standard': [
+      'local'
+    ],
+    'provider': [
+      'local'
+    ]
+  },
+  'signals:add_exchange': {
+    'standard': [
+      'local'
+    ],
+    'provider': [
+      'local'
+    ]
+  },
+  'signals:add_ip': {
+    'standard': [
+      'local'
+    ],
+    'provider': [
+      'local'
+    ]
+  },
+  'signals:add_provider': {
+    'standard': [
+      'local'
+    ],
+    'provider': [
+      'local'
+    ]
+  },
+  'signals:get_providers': {
+    'standard': [
+      'local'
+    ],
+    'provider': [
+      'local'
+    ]
+  },
+  'signals:send': {
+    'standard': [
+      'providerwhitelist'
+    ],
+    'provider': [
+      'providerwhitelist'
+    ]
+  },
+  'signals:remove_admin': {
+    'standard': [
+      'local'
+    ],
+    'provider': [
+      'local'
+    ]
+  },
+  'signals:remove_exchange': {
+    'standard': [
+      'local'
+    ],
+    'provider': [
+      'local'
+    ]
+  },
+  'signals:remove_ip': {
+    'standard': [
+      'local'
+    ],
+    'provider': [
+      'local'
+    ]
+  },
+  'symbolmap:add': {
+    'standard': [
+      'local'
+    ],
+    'provider': [
+      'local'
+    ]
+  },
+  'symbolmap:delete': {
+    'standard': [
+      'local'
+    ],
+    'provider': [
+      'local'
+    ]
+  },
+  'symbolmap:get': {
+    'standard': [
+      'local'
+    ],
+    'provider': [
+      'local'
+    ]
+  },
+  'trade:balances': {
+    'standard': [
+      'core','singleuser',
+      'multiuser','user',
+      'token'
+    ],
+    'provider': [
+      'token'
+    ]
+  },
+  'trade:buy': {
+    'standard': [
+      'core','singleuser',
+      'multiuser','user',
+      'token'
+    ],
+    'provider': [
+      'local'
+    ]
+  },
+  'trade:cancel': {
+    'standard': [
+      'core','singleuser',
+      'multiuser','user',
+      'token'
+    ],
+    'provider': [
+      'local'
+    ]
+  },
+  'trade:cancelall': {
+    'standard': [
+      'core','singleuser',
+      'multiuser','user',
+      'token'
+    ],
+    'provider': [
+      'local'
+    ]
+  },
+  'trade:close': {
+    'standard': [
+      'core','singleuser',
+      'multiuser','user',
+      'token'
+    ],
+    'provider': [
+      'local',
+      'token'
+    ]
+  },
+  'trade:closeall': {
+    'standard': [
+      'core','singleuser',
+      'multiuser','user',
+      'token'
+    ],
+    'provider': [
+      'local',
+      'token'
+    ]
+  },
+  'trade:leverage': {
+    'standard': [
+      'core','singleuser',
+      'multiuser','user',
+      'token'
+    ],
+    'provider': [
+      'local'
+    ]
+  },
+  'trade:long': {
+    'standard': [
+      'core','singleuser',
+      'multiuser','user',
+      'token'
+    ],
+    'provider': [
+      'local'
+    ]
+  },
+  'trade:market': {
+    'standard': [
+      'core','singleuser',
+      'multiuser','user',
+      'token'
+    ],
+    'provider': []
+  },
+  'trade:markets': {
+    'standard': [
+      'core','singleuser',
+      'multiuser','user',
+      'token'
+    ],
+    'provider': []
+  },
+  'trade:order': {
+    'standard': [
+      'core','singleuser',
+      'multiuser','user',
+      'token'
+    ],
+    'provider': []
+  },
+  'trade:order_history': {
+    'standard': [
+      'core','singleuser',
+      'multiuser','user',
+      'token'
+    ],
+    'provider': []
+  },
+  'trade:orders': {
+    'standard': [
+      'core','singleuser',
+      'multiuser','user',
+      'token'
+    ],
+    'provider': []
+  },
+  'trade:pnl': {
+    'standard': [
+      'core','singleuser',
+      'multiuser','user',
+      'token'
+    ],
+    'provider': []
+  },
+  'trade:position': {
+    'standard': [
+      'core','singleuser',
+      'multiuser','user',
+      'token'
+    ],
+    'provider': []
+  },
+  'trade:positions': {
+    'standard': [
+      'core','singleuser',
+      'multiuser','user',
+      'token'
+    ],
+    'provider': []
+  },
+  'trade:sell': {
+    'standard': [
+      'core','singleuser',
+      'multiuser','user',
+      'token'
+    ],
+    'provider': [
+      'local'
+    ]
+  },
+  'trad
