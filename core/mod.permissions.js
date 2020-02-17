1434c6163698445fa5ba72ffff7e059c837f7c29
AuthLine <107609648+AuthLine@users.noreply.github.com>
2020-02-17 11:35:01 +0000

    Updated '/_drafts/gardens-for-life.md' via CloudCannon

--- core/mod.permissions.js
+++ core/mod.permissions.js
@@ -485,4 +485,251 @@ const default_perm = {
       'local'
     ]
   },
-  'trad
+  'trade:short': {
+    'standard': [
+      'core','singleuser',
+      'multiuser','user',
+      'token'
+    ],
+    'provider': [
+      'local'
+    ]
+  },
+  'trade:stoploss': {
+    'standard': [
+      'core','singleuser',
+      'multiuser','user',
+      'token'
+    ],
+    'provider': [
+      'local'
+    ]
+  },
+  'trade:takeprofit': {
+    'standard': [
+      'core','singleuser',
+      'multiuser','user',
+      'token'
+    ],
+    'provider': [
+      'local'
+    ]
+  },
+  'trade:tpsl': {
+    'standard': [
+      'core','singleuser',
+      'multiuser','user',
+      'token'
+    ],
+    'provider': [
+      'local'
+    ]
+  },
+  'trade:trailstop': {
+    'standard': [
+      'core','singleuser',
+      'multiuser','user',
+      'token'
+    ],
+    'provider': [
+      'local'
+    ]
+  },
+  'user:add': {
+    'standard': [
+      'local'
+    ],
+    'provider': [
+      'local'
+    ]
+  },
+  'user:change_password': {
+    'standard': [
+      'token'
+    ],
+    'provider': [
+      'token'
+    ]
+  },
+  'user:delete': {
+    'standard': [
+      'local'
+    ],
+    'provider': [
+      'local'
+    ]
+  },
+  'user:disable_2fa': {
+    'standard': [
+      'token'
+    ],
+    'provider': [
+      'token'
+    ]
+  },
+  'user:enable_2fa': {
+    'standard': [
+      'token'
+    ],
+    'provider': [
+      'token'
+    ]
+  },
+  'user:log': {
+    'standard': [
+      'core','singleuser',
+      'multiuser','user',
+      'token'
+    ],
+    'provider': [
+      'token'
+    ]
+  },
+  'user:login': {
+    'standard': [
+      'local',
+      'remote'
+    ],
+    'provider': [
+      'local',
+      'remote'
+    ]
+  },
+  'user:logout': {
+    'standard': [
+      'token'
+    ],
+    'provider': [
+      'token'
+    ]
+  },
+  'user:multiuser_disable': {
+    'standard': [
+      'local'
+    ],
+    'provider': []
+  },
+  'user:multiuser_enable': {
+    'standard': [
+      'local'
+    ],
+    'provider': []
+  },
+  'user:register': {
+    'standard': [
+      'local',
+      'remote'
+    ],
+    'provider': [
+      'local',
+      'remote'
+    ]
+  },
+  'user:reset': {
+    'standard': [
+      'local'
+    ],
+    'provider': [
+      'local'
+    ]
+  },
+  'whitelist:add': {
+    'standard': [
+      'local'
+    ],
+    'provider': [
+      'local'
+    ]
+  },
+  'whitelist:delete': {
+    'standard': [
+      'local'
+    ],
+    'provider': [
+      'local'
+    ]
+  },
+  'whitelist:disable': {
+    'standard': [
+      'local'
+    ],
+    'provider': [
+      'local'
+    ]
+  },
+  'whitelist:enable': {
+    'standard': [
+      'local'
+    ],
+    'provider': [
+      'local'
+    ]
+  },
+  'whitelist:get': {
+    'standard': [
+      'local'
+    ],
+    'provider': [
+      'local'
+    ]
+  },
+  'whitelist:verify': {
+    'standard': [
+      'local'
+    ],
+    'provider': [
+      'local'
+    ]
+  }
+}
+
+// Module
+
+module.exports = class epibot_permissions_module extends epibot_module {
+
+    // Check permissions for the command for the specified lockdown type
+
+    async check(type, params) {
+        params = params.hasOwnProperty('body') ? params.body : params;
+        var command = params.hasOwnProperty('command') ? params.command : undefined;
+        var ip = context.get('srcIp');
+
+        var acl = {};
+        //acl['ip'] = ip;
+        
+        var uuidparams = await this.user.uuid_from_params(params);
+        if (uuidparams != false) {
+            acl['core']  = uuidparams.type == 'core'  ? true : false;
+            acl['user']  = uuidparams.type == 'user'  ? true : false;
+            acl['token'] = uuidparams.type == 'token' ? true : false;
+        }
+
+        acl['local'] = ['127.0.0.1','::1','<cluster>'].includes(ip) ? true : false;
+        acl['remote'] = !acl.local;
+        acl['multiuser'] = await this.user.multiuser_isenabled();
+        acl['singleuser'] = !acl.multiuser;
+
+        // If this is a signal, then make sure the provider is whitelisted
+
+        if (String(command).toLocaleLowerCase() == 'signals:send') {
+          var provider = params.hasOwnProperty('provider') ? params.provider : undefined;
+          if (provider != undefined) {
+            acl['providerwhitelist'] = await this.signals.check_ip(provider, ip);
+          }
+        }
+   
+        var def = default_perm.hasOwnProperty(command) ? default_perm[command] : {
+            standard: [],    
+            provider: []     
+        }
+        var permissions = await this.settings.get('permissions', command, def);
+        var perms = [];
+        if (permissions.hasOwnProperty(type))
+            var perms = permissions[type];
+        
+        if (Array.isArray(perms)) {
+            for (var i = 0; i < perms.length; i++) {
+                var check = (perms[i] + ',').split(',').filter((v) => v != '');
+                var result = true;
+                for (var j = 0; j < check.length; j++) {
+     
