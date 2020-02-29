a0a51ec49e3cfdd4c637cb8e6ba509e247fef9ca
AuthLine <107609648+AuthLine@users.noreply.github.com>
2020-02-29 12:26:05 +0000

    add to README

--- database/init/init.mysql.sql
+++ database/init/init.mysql.sql
@@ -96,4 +96,50 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 
 DROP TRIGGER IF EXISTS `users_before_insert`;
-DROP TRIGGER IF 
+DROP TRIGGER IF EXISTS `users_before_update`;
+
+DELIMITER $$
+CREATE TRIGGER `users_before_insert` BEFORE INSERT ON `users`
+FOR EACH ROW
+  IF new.uuid IS NULL
+  THEN
+    SET new.uuid = uuid_v4();
+  END IF$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE TRIGGER `users_before_update` BEFORE UPDATE ON `users`
+FOR EACH ROW
+  SET new.last = CURRENT_TIMESTAMP;
+$$
+DELIMITER ;
+
+--
+-- Table structure for table `signals`
+--
+
+CREATE TABLE `signals` (
+  `uid` INT NOT NULL AUTO_INCREMENT,
+  `timestamp` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `provider` VARCHAR(36) NOT NULL,
+  `user` VARCHAR(36) NOT NULL,
+  `exchange` VARCHAR(50) NOT NULL,
+  `stub` VARCHAR(50) NOT NULL,
+  `symbol` VARCHAR(50) NOT NULL,
+  `signal` VARCHAR(50) NOT NULL,
+  `result` TINYINT NOT NULL,
+  `message` TEXT NOT NULL,
+  PRIMARY KEY (`uid`),
+  UNIQUE INDEX `uid_UNIQUE` (`uid` ASC) VISIBLE,
+  INDEX `IDX_PROVIDER` (`provider` ASC) INVISIBLE,
+  INDEX `IDX_USER` (`user` ASC) VISIBLE,
+  INDEX `IDX_TIMESTAMP` (`timestamp` ASC) INVISIBLE,
+  INDEX `IDX_RESULT` (`result` ASC) VISIBLE);
+
+
+
+--
+-- Update version
+--
+
+REPLACE INTO `settings` (`mainkey`, `subkey`, `value`) VALUES ('core', 'mysql:dbver', '2');
