a08c6f15cbb6fa93c2f4c29a6442b0fcdb7acdfd
AuthLine <107609648+AuthLine@users.noreply.github.com>
2020-04-05 09:18:47 +0000

    Update dependencies to latest versions

---
+++ views/assets/libraries/jqwidgets/jqxbuttongroup.js
@@ -0,0 +1,8 @@
+/*
+jQWidgets v11.0.1 (2020-Dec)
+Copyright (c) 2011-2020 jQWidgets.
+License: https://jqwidgets.com/license/
+*/
+/* eslint-disable */
+
+(function(a){a.jqx.jqxWidget("jqxButtonGroup","",{});a.extend(a.jqx._jqxButtonGroup.prototype,{defineInstance:function(){var b={mode:"default",roundedCorners:true,disabled:false,enableHover:false,orientation:"horizontal",width:null,height:null,_eventsMap:{mousedown:a.jqx.mobile.getTouchEvent
