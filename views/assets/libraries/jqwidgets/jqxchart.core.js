37c972a45688f48cb9cdc7d044982e0e41b1d805
AuthLine <107609648+AuthLine@users.noreply.github.com>
2020-05-14 20:21:53 +0000

    Update chart.html

---
+++ views/assets/libraries/jqwidgets/jqxchart.core.js
@@ -0,0 +1,8 @@
+/*
+jQWidgets v11.0.1 (2020-Dec)
+Copyright (c) 2011-2020 jQWidgets.
+License: https://jqwidgets.com/license/
+*/
+/* eslint-disable */
+
+(function(a){a.jqx.jqxWidget("jqxChart","",{});a.extend(a.jqx._jqxCha
