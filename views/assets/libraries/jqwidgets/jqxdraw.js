90c8ef63f36302586f6229699b440af5c2d7856f
AuthLine <107609648+AuthLine@users.noreply.github.com>
2021-05-16 10:11:34 +0000

    New.

---
+++ views/assets/libraries/jqwidgets/jqxdraw.js
@@ -0,0 +1,8 @@
+/*
+jQWidgets v11.0.1 (2020-Dec)
+Copyright (c) 2011-2020 jQWidgets.
+License: https://jqwidgets.com/license/
+*/
+/* eslint-disable */
+
+(function(a){a.jqx.jqxWidget("jqxDraw","",{});a.extend(a.jqx._jqxDraw.prototype,{defineInstance:function(){var c={renderEngine:""};a.extend(true,this,c);var d=["clear","on","off","removeElement","attr","getAttr","line","circle","rect","path","pieslice","text","measureText"];for(var b in d){this._addFn(a.jqx._jqxDraw.prototype,d[b])}if(
