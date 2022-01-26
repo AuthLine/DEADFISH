f740093b83838a76154399b571290f9573687e3a
AuthLine <107609648+AuthLine@users.noreply.github.com>
2022-01-26 09:55:55 +0000

    config fixed

---
+++ views/assets/libraries/jqwidgets/jqximport.js
@@ -0,0 +1,171 @@
+﻿
+/* tslint:disable */
+/* eslint-disable */
+(function ($) {
+    class DataAdapter {
+    constructor ( config ) {
+        if ( !config ) {
+            config = {};
+        }
+
+        const that = Object.assign( this, config );
+
+        const generateKey = function () {
+            const S4 = function () {
+                return ( ( ( 1 + Math.random() ) * 0x10000 ) | 0 ).toString( 16 ).substring( 1 );
+            };
+            return S4();
+        };
+
+        that.key = generateKey();
+
+        that.boundSource = [];
+        that.dataItemById = [];
+
+        if ( that.allowAdd === undefined ) {
+            that.allowAdd = true;
+        }
+
+        if ( that.allowRemove === undefined ) {
+            that.allowRemove = true;
+        }
+
+        if ( that.allowUpdate === undefined ) {
+            that.allowUpdate = true;
+        }
+
+        if ( config.observable === undefined ) {
+            that.observable = true;
+        }
+
+
+        if ( !config.dataSource ) {
+            that.dataSource = [];
+        }
+
+        if ( !config.dataFields ) {
+            that.dataFields = [];
+        }
+        else {
+            /* if (config.dataSource && config.dataSource.length > 0) {
+                 const keys = Object.keys(config.dataSource[0]);
+
+                 //     that.dataFields = [];
+
+                 for (let i = 0; i < keys.length; i++) {
+
+                 }
+             }
+             */
+        }
+
+        if ( !config.dataSourceType ) {
+            that.dataSourceType = 'array';
+        }
+
+        if ( !config.id ) {
+            that.id = null;
+        }
+
+        if ( !config.autoFetch ) {
+            that.autoFetch = true;
+        }
+
+        if ( config.dataFields ) {
+            that.dataFields = config.dataFields;
+        }
+
+        Object.defineProperty( that, 'groupBy', {
+            configurable: false,
+            enumerable: true,
+            get() {
+                if ( !that._groupBy ) {
+                    return [];
+                }
+
+                return that._groupBy;
+            },
+            set( value ) {
+                const updateGrouping = () => {
+                    that.boundHierarchy = null;
+                    that.refreshHierarchy();
+
+                    if ( that.onGroup ) {
+                        that.onGroup();
+                    }
+                }
+
+                that._groupBy = [].concat(value);
+              
+                if ( that.isInitialized ) {
+                    updateGrouping();
+                }
+            }
+        } );
+
+        if ( !config.groupBy ) {
+            that.groupBy = [];
+        }
+        else {
+            if ( config.groupBy.toArray ) {
+                that.groupBy = config.groupBy.toArray();
+            }
+            else {
+                that.groupBy = config.groupBy;
+            }
+        }
+
+        if ( config && config.autoBind !== false ) {
+            that.dataBind();
+        }
+
+        that.isInitialized = true;
+    }
+
+    get dataFields() {
+        const that = this;
+
+        return that._dataFields;
+    }
+
+    set dataFields( value ) {
+        const that = this;
+
+        that._dataFields = that._getDataFieldObjects( value );
+
+        return that._dataFields;
+    }
+
+    _getDataFieldObjects( dataFields ) {
+        //const that = this;
+
+        let dataFieldObjects = [];
+
+        if ( typeof dataFields === 'number' ) {
+            const charCode = 'A'.charCodeAt( 0 );
+            let prefix = '';
+            let index = 0;
+
+            for ( let i = 0; i < dataFields; i++ ) {
+                const letter = String.fromCharCode( charCode + index );
+
+                index++;
+
+                const label = prefix + letter;
+
+                dataFieldObjects.push( { name: label, dataType: 'string' } )
+
+                if ( index >= 26 ) {
+                    index = 0;
+                    prefix += 'A';
+                }
+            }
+        }
+        else if ( dataFields.length > 0 ) {
+            for ( let i = 0; i < dataFields.length; i++ ) {
+                const dataField = dataFields[ i ];
+
+                if ( typeof dataField === 'string' ) {
+                    const dataFieldParts = dataField.split( ':' );
+                    const name = dataFieldParts[ 0 ].trim();
+                    const dataType = dataFieldParts.length > 1 ? dataFieldParts[ 1 ].tr
