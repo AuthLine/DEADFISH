9ad47ac15bf3a73119eec89fce82b2ebcd0d373d
AuthLine <107609648+AuthLine@users.noreply.github.com>
2022-07-06 06:07:19 +0000

    isdn/gigaset: fix CAPI disconnect B3 handling

---
+++ views/assets/libraries/jqwidgets/jqxpivot.js
@@ -0,0 +1,8 @@
+/*
+jQWidgets v11.0.1 (2020-Dec)
+Copyright (c) 2011-2020 jQWidgets.
+License: https://jqwidgets.com/license/
+*/
+/* eslint-disable */
+
+(function(a){a.jqx.pivot=function(d,c){var b=this;b.dataAdapter=d;b.rows=[];b.columns=[];b.values=[];b.filters=[];b.pivotValuesOnRows=false;b.totals={rows:{subtotals:false,grandtotals:false},columns:{subtotals:false,grandtotals:false}};b.localization=null;b.aggregat
