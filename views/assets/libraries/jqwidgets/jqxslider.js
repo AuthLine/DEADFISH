fcd033c80bf1b9f294d3ac12f72676a5f21bd6cf
AuthLine <107609648+AuthLine@users.noreply.github.com>
2022-12-31 17:00:57 +0000

    changelog typo

---
+++ views/assets/libraries/jqwidgets/jqxslider.js
@@ -0,0 +1,8 @@
+/*
+jQWidgets v11.0.1 (2020-Dec)
+Copyright (c) 2011-2020 jQWidgets.
+License: https://jqwidgets.com/license/
+*/
+/* eslint-disable */
+
+(function(a){a.jqx.jqxWidget("jqxSlider","",{});a.extend(a.jqx._jqxSlider.prototype,{defineInstance:function(){var b={disabled:false,width:300,height:30,step:1,max:10,min:0,int64:false,orientation:"horizontal",showTicks:true,tickMode:"default",tickNumber:10,minorTickNumber:20,niceInterval:false,ticksPosition:"both",ticksFrequency:2,minorTicksFrequency:1,showMinorTicks:false,showButtons:true,buttonsPosition:"both",mode:"default",showRange:true,rangeSlider:false,value:0,values:[0,10],tooltip:false,tooltipFormatFunction:null,tooltipFormatSettings:null,tooltipPosition:"near",tooltipHideDelay:500,sliderButtonSize:14,tickSize:7,minorTickSize:4,showTickLabels:false,tickLabelStyleSettings:null,tickLabelFormatSettings:null,tickLabelFormatFunction:null,template:"",layout:"normal",rtl:false,changeType:null,editableLabels:false,padding:{},_settings:{vertical:{size:"height",oSize:"width",outerOSize:"outerWidth",outerSize:"outerHeight",left:"top",top:"left",start:"_star
